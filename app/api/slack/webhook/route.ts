dd07c1b3baee04b28fc7f6cc521d5703b1d38912
Nathan Rajlich <n@n8.io>
2025-08-11 10:53:52 -0700

    .

--- app/api/slack/webhook/route.ts
+++ app/api/slack/webhook/route.ts
@@ -1,8 +1,15 @@
 export async function POST(req: Request) {
 	const body = await req.json();
 	console.log(body);
+
+	// Slack Events API URL Verification
 	if (body.type === "url_verification") {
-		return new Response(body.challenge);
+		return new Response(body.challenge, {
+			headers: {
+				"Content-Type": "text/plain",
+			},
+		});
 	}
+
 	return new Response("Hello, world!");
 }
