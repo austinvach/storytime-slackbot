019232c665c7c0025091fe4829e4870237bcd373
Nathan Rajlich <n@n8.io>
2025-08-12 13:40:53 -0700

    Use `waitUntil()` instead of `after()`

--- app/api/slack/command/route.ts
+++ app/api/slack/command/route.ts
@@ -1,15 +1,17 @@
+import { waitUntil } from "@vercel/functions";
 import { start } from "@vercel/workflow-core/runtime";
-import { after } from "next/server";
 
 export async function POST(req: Request) {
 	const rawBody = await req.text();
 	const formData = new URLSearchParams(rawBody);
 
-	after(async () => {
-		console.log("Starting Storytime workflow");
-		const w = await start("storytime", [formData]);
-		console.log(w);
-	});
+	waitUntil(
+		(async () => {
+			console.log("Starting Storytime workflow");
+			const w = await start("storytime", [formData]);
+			console.log(w);
+		})(),
+	);
 
 	return new Response(`Let's create a story!`);
 }
