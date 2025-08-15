798a529c473e461d44e4c68054be45e877d80b5b
Nathan Rajlich <n@n8.io>
2025-08-15 15:43:31 -0700

    Moar debug

--- app/api/slack/webhook/route.ts
+++ app/api/slack/webhook/route.ts
@@ -1,7 +1,12 @@
 import { handleWebhook } from "@vercel/workflow-core/runtime";
 
 export async function POST(req: Request) {
+	console.log("Slack webhook received");
+	console.log(1, req.body, req.bodyUsed);
+
 	const body = await req.clone().json();
+
+	console.log(2, req.body, req.bodyUsed);
 
 	console.log(body);
 
